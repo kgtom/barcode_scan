60eabfcbaed3696f3be4d14fc680f4d1769d41e2
tom <leiweizhen@zailushang.cn>
2017-07-05 11:51:23 +0800

    add scan barcode by zxing lib

---
+++ Barcode_scanbyZXing/Barcode_scan/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Barcode_scan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.barCodeImg = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Read1DBtn = new System.Windows.Forms.Button();
+            this.OpenImgBtn = new System.Windows.Forms.Button();
+            this.ImgPathTxt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ContentTxt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Create1DBtn = new System.Windows.Forms.Button();
+            this.btn_test = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.barCodeImg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // barCodeImg
+            // 
+            this.barCodeImg.Location = new System.Drawing.Point(12, 109);
+            this.barCodeImg.Name = "barCodeImg";
+            this.barCodeImg.Size = new System.Drawing.Size(1336, 464);
+            this.barCodeImg.TabIndex = 22;
+            this.barCodeImg.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "图片：";
+            // 
+            // Read1DBtn
+            // 
+            this.Read1DBtn.Location = new System.Drawing.Point(442, 30);
+            this.Read1DBtn.Name = "Read1DBtn";
+            this.Read1DBtn.Size = new System.Drawing.Size(75, 23);
+            this.Read1DBtn.TabIndex = 19;
+            this.Read1DBtn.Text = "读取条形码";
+            this.Read1DBtn.UseVisualStyleBackColor = true;
+            this.Read1DBtn.Click += new System.EventHandler(this.Read1DBtn_Click);
+            // 
+            // OpenImgBtn
+            // 
+            this.OpenImgBtn.Location = new System.Drawing.Point(272, 57);
+            this.OpenImgBtn.Name = "OpenImgBtn";
+            this.OpenImgBtn.Size = new System.Drawing.Size(66, 23);
+            this.OpenImgBtn.TabIndex = 18;
+            this.OpenImgBtn.Text = "打开图片";
+            this.OpenImgBtn.UseVisualStyleBackColor = true;
+            this.OpenImgBtn.Click += new System.EventHandler(this.OpenImgBtn_Click);
+            // 
+            // ImgPathTxt
+            // 
+            this.ImgPathTxt.Location = new System.Drawing.Point(69, 59);
+            this.ImgPathTxt.Name = "ImgPathTxt";
+            this.ImgPathTxt.ReadOnly = true;
+            this.ImgPathTxt.Size = new System.Drawing.Size(197, 21);
+            this.ImgPathTxt.TabIndex = 17;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "图片路径：";
+            // 
+            // ContentTxt
+            // 
+            this.ContentTxt.Location = new System.Drawing.Point(69, 30);
+            this.ContentTxt.Name = "ContentTxt";
+            this.ContentTxt.Size = new System.Drawing.Size(269, 21);
+            this.ContentTxt.TabIndex = 14;
+            this.ContentTxt.Text = "123456";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "条码内容：";
+            // 
+            // Create1DBtn
+            // 
+            this.Create1DBtn.Location = new System.Drawing.Point(361, 30);
+            this.Create1DBtn.Name = "Create1DBtn";
+            this.Create1DBtn.Size = new System.Drawing.Size(75, 23);
+            this.Create1DBtn.TabIndex = 12;
+            this.Create1DBtn.Text = "生成条形码";
+            this.Create1DBtn.UseVisualStyleBackColor = true;
+            this.Create1DBtn.Click += new System.EventHandler(this.Create1DBtn_Click);
+            // 
+            // btn_test
+            // 
+            this.btn_test.Location = new System.Drawing.Point(361, 65);
+            this.btn_test.Name = "btn_test";
+            this.btn_test.Size = new System.Drawing.Size(75, 23);
+            this.btn_test.TabIndex = 23;
+            this.btn_test.Text = "test";
+            this.btn_test.UseVisualStyleBackColor = true;
+            this.btn_test.Click += new System.EventHandler(this.btn_test_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1383, 585);
+            this.Controls.Add(this.btn_test);
+            this.Controls.Add(this.barCodeImg);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Read1DBtn);
+            this.Controls.Add(this.OpenImgBtn);
+            this.Controls.Add(this.ImgPathTxt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ContentTxt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Create1DBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.barCodeImg)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox barCodeImg;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button Read1DBtn;
+        private System.Windows.Forms.Button OpenImgBtn;
+        private System.Windows.Forms.TextBox ImgPathTxt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox ContentTxt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Create1DBtn;
+        private System.Windows.Forms.Button btn_test;
+    }
+}
+
---
+++ Barcode_scanbyZXing/Barcode_scan/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZXing;
+using ZXing.Common;
+
+namespace Barcode_scan
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // 生成条形码
+        private void Create1DBtn_Click(object sender, EventArgs e)
+        {
+
+            Regex rg = new Regex("^[0-9]*$");
+            if (!rg.IsMatch(this.ContentTxt.Text))
+            {
+                MessageBox.Show("需要输入数字");
+                return;
+            }
+
+
+            // 1.设置条形码规格
+            EncodingOptions encodeOption = new EncodingOptions();
+            encodeOption.Height = 130; // 必须制定高度、宽度
+            encodeOption.Width = 240;
+
+            // 2.生成条形码图片并保存
+            ZXing.BarcodeWriter wr = new BarcodeWriter();
+            wr.Options = encodeOption;
+            wr.Format = BarcodeFormat.CODE_39; //  条形码规格
+            Bitmap img = wr.Write(this.ContentTxt.Text); // 生成图片
+            string filePath = System.AppDomain.CurrentDomain.BaseDirectory + "\\barcode-" + this.ContentTxt.Text + ".jpg";
+            img.Save(filePath, System.Drawing.Imaging.ImageFormat.Jpeg);
+
+            // 3.读取保存的图片
+            this.ImgPathTxt.Text = filePath;
+            this.barCodeImg.Image = img;
+            MessageBox.Show("保存成功：" + filePath);
+        }
+
+        // 读取条形码
+        private void Read1DBtn_Click(object sender, EventArgs e)
+        {
+            // 1.设置读取条形码规格
+            DecodingOptions decodeOption = new DecodingOptions();
+            decodeOption.PossibleFormats = new List<BarcodeFormat>() {
+               BarcodeFormat.All_1D,
+
+            };
+
+
+            // 2.进行读取操作
+            ZXing.BarcodeReader br = new BarcodeReader();
+            br.Options = decodeOption;
+
+            ZXing.Result rs = br.Decode(this.barCodeImg.Image as Bitmap);
+            if (rs == null)
+            {
+                this.ContentTxt.Text = "读取失败";
+                MessageBox.Show("读取失败");
+            }
+            else
+            {
+                this.ContentTxt.Text = rs.Text;
+                MessageBox.Show("读取成功，内容：" + rs.Text);
+            }
+        }
+
+        // 【打开图片】
+        private void OpenImgBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.InitialDirectory = System.AppDomain.CurrentDomain.BaseDirectory;
+            fileDialog.Filter = "图形文件(*.jpg)|*.jpg";
+            if (fileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string filePath = fileDialog.FileName;
+                this.ImgPathTxt.Text = filePath;
+                this.barCodeImg.Image = Bitmap.FromFile(filePath);
+            }
+        }
+
+        private void btn_test_Click(object sender, EventArgs e)
+        {
+            string filePath = System.AppDomain.CurrentDomain.BaseDirectory + "\\barcode-" + "test001" + ".jpg";
+
+            // CaptureImage(this.ImgPathTxt.Text, filePath, 240, 75, 420, -5);
+
+
+
+           
+
+
+
+
+            //this.ImgPathTxt.Text = @"C:\Users\tom\Desktop\行程单\2017-06-22\001.jpg";
+            //  CutForSquare(FileToStream(this.ImgPathTxt.Text), filePath, 800, 80);
+
+            //CutForCustom(FileToStream(this.ImgPathTxt.Text), filePath, 240, 130, 80);
+        }
+
+        public byte[] ImageToByteArray(System.Drawing.Image imageIn)
+        {
+            using (var ms = new MemoryStream())
+            {
+                imageIn.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                return ms.ToArray();
+            }
+        }
+
+        public Stream FileToStream(string fileName)
+        {
+            // 打开文件 
+            FileStream fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.Read);
+            // 读取文件的 byte[] 
+            byte[] bytes = new byte[fileStream.Length];
+            fileStream.Read(bytes, 0, bytes.Length);
+            fileStream.Close();
+            // 把 byte[] 转换成 Stream 
+            Stream stream = new MemoryStream(bytes);
+            return stream;
+        }
+
+        /// <summary>
+        /// 截取图片方法
+        /// </summary>
+        /// <param name="url">图片地址</param>
+        /// <param name="beginX">开始位置-X</param>
+        /// <param name="beginY">开始位置-Y</param>
+        /// <param name="getX">截取宽度</param>
+        /// <param name="getY">截取长度</param>
+        /// <param name="fileName">文件名称</param>
+        /// <param name="savePath">保存路径</param>
+        /// <param name="fileExt">后缀名</param>
+        public static void CaptureImage(string sFromFilePath, string saveFilePath, int width, int height, int spaceX, int spaceY)
+        {
+            //载入底图   
+            Image fromImage = Image.FromFile(sFromFilePath);
+            int x = 0;   //截取X坐标   
+            int y = 0;   //截取Y坐标   
+            //原图宽与生成图片宽   之差       
+            //当小于0(即原图宽小于要生成的图)时，新图宽度为较小者   即原图宽度   X坐标则为0     
+            //当大于0(即原图宽大于要生成的图)时，新图宽度为设置值   即width         X坐标则为   sX与spaceX之间较小者   
+            //Y方向同理   
+            int sX = fromImage.Width - width;
+            int sY = fromImage.Height - height;
+            if (sX > 0)
+            {
+                x = sX > spaceX ? spaceX : sX;
+            }
+            else
+            {
+                width = fromImage.Width;
+            }
+            if (sY > 0)
+            {
+                y = sY > spaceY ? spaceY : sY;
+            }
+            else
+            {
+                height = fromImage.Height;
+            }
+
+
+            //创建新图位图   
+            Bitmap bitmap = new Bitmap(width, height);
+            //创建作图区域   
+            Graphics graphic = Graphics.FromImage(bitmap);
+            //截取原图相应区域写入作图区   
+            graphic.DrawImage(fromImage, 0, 0, new Rectangle(x, y,350, 90), GraphicsUnit.Point);
+
+            //设置质量
+            graphic.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.High;
+            graphic.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            //graphic.Clear(Color.White);
+
+            //从作图区生成新图   
+            Image saveImage = Image.FromHbitmap(bitmap.GetHbitmap());
+
+
+            //保存图象   
+            //saveImage.Save(saveFilePath, ImageFormat.Jpeg);
+
+            //关键质量控制
+            //获取系统编码类型数组,包含了jpeg,bmp,png,gif,tiff
+            ImageCodecInfo[] icis = ImageCodecInfo.GetImageEncoders();
+            ImageCodecInfo ici = null;
+            foreach (ImageCodecInfo i in icis)
+            {
+                if (i.MimeType == "image/jpeg" || i.MimeType == "image/bmp" || i.MimeType == "image/png" || i.MimeType == "image/gif")
+                {
+                    ici = i;
+                }
+            }
+            EncoderParameters ep = new EncoderParameters(1);
+            ep.Param[0] = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, (long)200);
+            saveImage.Save(saveFilePath, ici, ep);
+            //释放资源   
+            saveImage.Dispose();
+            bitmap.Dispose();
+            graphic.Dispose();
+        }
+
+        
+
+    }
+}
