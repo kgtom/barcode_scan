d9868d2c30fac9e8d70629a42e44b2b81eb9cde2
tom <leiweizhen@zailushang.cn>
2017-07-05 18:35:21 +0800

    add scan barcode by zbar lib

---
+++ barcodebyZBar/barcode/BarCode.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace barcode
+{
+    partial class BarCode
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_test = new System.Windows.Forms.Button();
+            this.barCodeImg = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtImgPath = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtImgName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.btnSelectPath = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtBarcode = new System.Windows.Forms.TextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.barCodeImg)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_test
+            // 
+            this.btn_test.Location = new System.Drawing.Point(613, 8);
+            this.btn_test.Name = "btn_test";
+            this.btn_test.Size = new System.Drawing.Size(75, 23);
+            this.btn_test.TabIndex = 33;
+            this.btn_test.Text = "上传图片";
+            this.btn_test.UseVisualStyleBackColor = true;
+            this.btn_test.Click += new System.EventHandler(this.btn_test_Click);
+            // 
+            // barCodeImg
+            // 
+            this.barCodeImg.Location = new System.Drawing.Point(228, 99);
+            this.barCodeImg.Name = "barCodeImg";
+            this.barCodeImg.Size = new System.Drawing.Size(1315, 572);
+            this.barCodeImg.TabIndex = 32;
+            this.barCodeImg.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(233, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 31;
+            this.label3.Text = "图片：";
+            // 
+            // txtImgPath
+            // 
+            this.txtImgPath.Location = new System.Drawing.Point(364, 10);
+            this.txtImgPath.Name = "txtImgPath";
+            this.txtImgPath.ReadOnly = true;
+            this.txtImgPath.Size = new System.Drawing.Size(197, 21);
+            this.txtImgPath.TabIndex = 28;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(293, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 27;
+            this.label2.Text = "图片路径：";
+            // 
+            // txtImgName
+            // 
+            this.txtImgName.Location = new System.Drawing.Point(77, 14);
+            this.txtImgName.Name = "txtImgName";
+            this.txtImgName.ReadOnly = true;
+            this.txtImgName.Size = new System.Drawing.Size(197, 21);
+            this.txtImgName.TabIndex = 26;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "图片名称：";
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(20, 99);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(160, 388);
+            this.listView1.TabIndex = 34;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // btnSelectPath
+            // 
+            this.btnSelectPath.Location = new System.Drawing.Point(795, 12);
+            this.btnSelectPath.Name = "btnSelectPath";
+            this.btnSelectPath.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectPath.TabIndex = 35;
+            this.btnSelectPath.Text = "上传文件夹";
+            this.btnSelectPath.UseVisualStyleBackColor = true;
+            this.btnSelectPath.Click += new System.EventHandler(this.btnSelectPath_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(12, 560);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 88);
+            this.listBox1.TabIndex = 36;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.toolStripProgressBar1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 712);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1623, 25);
+            this.toolStrip1.TabIndex = 38;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripLabel1.Margin = new System.Windows.Forms.Padding(0, 3, 0, 2);
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(104, 20);
+            this.toolStripLabel1.Text = "请选择上传的文件";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 22);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 39;
+            this.label4.Text = "条形码";
+            // 
+            // txtBarcode
+            // 
+            this.txtBarcode.Location = new System.Drawing.Point(77, 50);
+            this.txtBarcode.Name = "txtBarcode";
+            this.txtBarcode.Size = new System.Drawing.Size(197, 21);
+            this.txtBarcode.TabIndex = 40;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(613, 54);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 41;
+            this.btnUpdate.Text = "更改条形码";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // BarCode
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1623, 737);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.txtBarcode);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnSelectPath);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btn_test);
+            this.Controls.Add(this.barCodeImg);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtImgPath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtImgName);
+            this.Controls.Add(this.label1);
+            this.Name = "BarCode";
+            this.Text = "条形码上传";
+            ((System.ComponentModel.ISupportInitialize)(this.barCodeImg)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_test;
+        private System.Windows.Forms.PictureBox barCodeImg;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtImgPath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtImgName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button btnSelectPath;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtBarcode;
+        private System.Windows.Forms.Button btnUpdate;
+    }
+}
+
---
+++ barcodebyZBar/barcode/BarCode.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace barcode
+{
+    public partial class BarCode : Form
+    {
+
+        List<FileInfo> uploadFileList = new List<FileInfo>();
+
+        public BarCode()
+        {
+            InitializeComponent();
+            // 加入这行
+            Control.CheckForIllegalCrossThreadCalls = false;
+            LoadListView();
+        }
+        /// <summary>  
+        /// 初始化上传列表  
+        /// </summary>  
+        void LoadListView()
+        {
+            listView1.View = View.Details;
+            listView1.GridLines = true;
+            listView1.FullRowSelect = true;
+            listView1.Columns.Add("文件名", 50, HorizontalAlignment.Center);
+            listView1.Columns.Add("条形码", 100, HorizontalAlignment.Center);
+
+        }
+       
+
+        /// <summary>
+        /// 上传图片
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_test_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Multiselect = true;
+            ofd.Filter = "img files (*.jpg)|*.jpg|All files (*.*)|*.*";
+            ofd.Multiselect = true;           
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    int i = 0;
+                    foreach (string fileName in ofd.FileNames)
+                    {
+                        FileInfo fi = new FileInfo(fileName);
+                        ListViewItem lvi = new ListViewItem(Path.GetFileNameWithoutExtension(fileName));
+                        lvi.Text = ofd.SafeFileNames[i];
+                        lvi.SubItems.Add(ScanBarCode(fileName));                     
+                        lvi.SubItems.Add(fileName);
+                        listView1.Items.Add(lvi);
+                        i++;
+
+                    }                  
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+            
+        }
+
+
+
+       
+        /// <summary>
+        /// 选择文件夹
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSelectPath_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog path = new FolderBrowserDialog();
+            path.ShowDialog();
+            this.txtImgPath.Text = path.SelectedPath;
+
+            //C#遍历指定文件夹中的所有文件 
+            DirectoryInfo theFolder = new DirectoryInfo(path.SelectedPath);
+            //遍历文件夹
+            foreach (DirectoryInfo NextFolder in theFolder.GetDirectories())
+            {
+
+                MessageBox.Show("请选择图片所在的文件夹");
+                return;
+            }
+
+
+
+            uploadFileList = theFolder.GetFiles().ToList();
+            this.toolStripProgressBar1.Visible = true;
+            this.toolStripProgressBar1.Maximum = uploadFileList.Count();
+            listView1.Items.Clear();
+            Task.Run(() =>
+            {
+
+               
+                BindDataLisView();
+
+            });
+        
+
+
+
+
+            //this.toolStripProgressBar1.Visible = true;
+            //this.toolStripProgressBar1.Maximum = theFolder.GetFiles().Count();
+            //int completeCount = 0;
+            ////遍历文件
+            //foreach (FileInfo NextFile in theFolder.GetFiles())
+            //{
+            //    //this.listBox2.Items.Add(NextFile.Name);
+            //    //this.listBox2.Items.Add(NextFile.FullName);
+
+            //    this.SetProgress(++completeCount);
+            //    this.SetInfoText(string.Format("正在努力处理第{1}条，数据：{0}", NextFile.Name, completeCount));
+
+            //    FileInfo fi = new FileInfo(NextFile.Name);
+            //    ListViewItem lvi = new ListViewItem(Path.GetFileNameWithoutExtension(NextFile.Name));
+            //    lvi.Tag = NextFile.Name;
+            //    lvi.SubItems.Add(ScanBarCode(NextFile.FullName));
+
+            //    lvi.SubItems.Add(NextFile.FullName);
+
+
+            //    listView1.Items.Add(lvi);
+
+            //}
+
+
+        }
+
+
+        //绑定 listView
+        private void BindDataLisView()
+        {
+           
+            int completeCount = 0;
+            int successCount = 0;
+
+            //this.BeginInvoke(new Action(() =>
+            //{
+            var start = DateTime.Now;
+
+            foreach (var NextFile in uploadFileList)
+                {
+
+                    if (listView1.Items.Count == 999)
+                    {
+                        Thread.Sleep(5000);
+                    }
+                    this.SetProgress(++completeCount);
+                this.SetInfoText(string.Format("正在努力处理第{1}条,共{2}条，数据：{0}", NextFile.Name, completeCount, uploadFileList.Count));
+
+                    FileInfo fi = new FileInfo(NextFile.Name);
+                    ListViewItem lvi = new ListViewItem(Path.GetFileNameWithoutExtension(NextFile.Name));
+                    lvi.Text = NextFile.Name;
+                var barcode = ScanBarCode(NextFile.FullName);
+                if (!string.IsNullOrEmpty(barcode))
+                {
+                    ++successCount;
+                }
+                    lvi.SubItems.Add(barcode);
+                    lvi.SubItems.Add(NextFile.FullName);
+                    listView1.Items.Add(lvi);
+
+                   
+                }
+
+            var stamp = DateTime.Now - start;
+            //}));
+            this.SetInfoText(string.Format("数据处理完成,成功扫描{0}条,未识别{1}条,耗时{2}s", successCount,uploadFileList.Count-successCount,Math.Floor(stamp.TotalSeconds)));
+        }
+
+        /// <summary>
+        /// 设置进度条文本
+        /// </summary>
+        /// <param name="text"></param>
+        private void SetInfoText(string text)
+        {
+            this.BeginInvoke(new Action(() =>
+            {
+                this.toolStripLabel1.Text = text;
+            }));
+        }
+
+        /// <summary>
+        /// 设置进度条百分比
+        /// </summary>
+        /// <param name="progress"></param>
+        private void SetProgress(int progress)
+        {
+            this.BeginInvoke(new Action(() =>
+            {
+                this.toolStripProgressBar1.Value = progress;
+            }));
+        }
+
+        // <summary>
+        /// 条码识别
+        /// </summary>
+        private string ScanBarCode(string fileName)
+        {
+            DateTime now = DateTime.Now;
+            Image primaryImage = Image.FromFile(fileName);
+
+            Bitmap pImg = MakeGrayscale3((Bitmap)primaryImage);
+            using (ZBar.ImageScanner scanner = new ZBar.ImageScanner())
+            {
+                scanner.SetConfiguration(ZBar.SymbolType.None, ZBar.Config.Enable, 0);
+                scanner.SetConfiguration(ZBar.SymbolType.CODE39, ZBar.Config.Enable, 1);
+                scanner.SetConfiguration(ZBar.SymbolType.CODE128, ZBar.Config.Enable, 1);
+
+                List<ZBar.Symbol> symbols = new List<ZBar.Symbol>();
+                symbols = scanner.Scan((Image)pImg);
+
+                if (symbols != null && symbols.Count > 0)
+                {
+                    string result = string.Empty;
+                    //this.ContentTxt.Text = symbols.FirstOrDefault().Data;
+                    return symbols.FirstOrDefault().Data;
+                    // symbols.ForEach(s => result += "条码内容:" + s.Data + " 条码质量:" + s.Quality + Environment.NewLine);
+                    //MessageBox.Show(result);
+                }
+                else
+                {
+                    //this.ContentTxt.Text = "未识别";
+                    return "";
+                }
+            }
+        }
+
+        /// <summary>
+        /// 处理图片灰度
+        /// </summary>
+        /// <param name="original"></param>
+        /// <returns></returns>
+        public static Bitmap MakeGrayscale3(Bitmap original)
+        {
+            //create a blank bitmap the same size as original
+            Bitmap newBitmap = new Bitmap(original.Width, original.Height);
+
+            //get a graphics object from the new image
+            Graphics g = Graphics.FromImage(newBitmap);
+
+            //create the grayscale ColorMatrix
+            System.Drawing.Imaging.ColorMatrix colorMatrix = new System.Drawing.Imaging.ColorMatrix(
+               new float[][]
+              {
+                 new float[] {.3f, .3f, .3f, 0, 0},
+                 new float[] {.59f, .59f, .59f, 0, 0},
+                 new float[] {.11f, .11f, .11f, 0, 0},
+                 new float[] {0, 0, 0, 1, 0},
+                 new float[] {0, 0, 0, 0, 1}
+              });
+
+            //create some image attributes
+            ImageAttributes attributes = new ImageAttributes();
+
+            //set the color matrix attribute
+            attributes.SetColorMatrix(colorMatrix);
+
+            //draw the original image on the new image
+            //using the grayscale color matrix
+            g.DrawImage(original, new Rectangle(0, 0, original.Width, original.Height),
+               0, 0, original.Width, original.Height, GraphicsUnit.Pixel, attributes);
+
+            //dispose the Graphics object
+            g.Dispose();
+            return newBitmap;
+        }
+
+        /// <summary>
+        /// 选中listview数据行
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (this.listView1.FocusedItem != null)
+            {
+                this.txtImgName.Text = this.listView1.FocusedItem.SubItems[0].Text;
+                this.barCodeImg.Image = Bitmap.FromFile(this.listView1.FocusedItem.SubItems[2].Text);
+                this.txtImgPath.Text = this.listView1.FocusedItem.SubItems[2].Text;
+                this.txtBarcode.Text = this.listView1.FocusedItem.SubItems[1].Text;
+            }
+        }
+
+    }
+}
